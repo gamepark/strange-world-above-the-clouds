e6a84a1531ae32de41a6e56cfae5d8429c9d3efc
Patrick Beaume <p.beaume@gmail.com>
2024-11-30 22:00:29 +0100

    remove limit on fumarole

--- app/src/locators/FumaroleStackLocator.ts
+++ app/src/locators/FumaroleStackLocator.ts
@@ -2,7 +2,6 @@ import { DeckLocator } from '@gamepark/react-game'
 
 export class FumaroleStackLocator extends DeckLocator {
   coordinates = { x: 8, y: -24 }
-  limit = 200
 }
 
 export const fumaroleStackLocator = new FumaroleStackLocator()
