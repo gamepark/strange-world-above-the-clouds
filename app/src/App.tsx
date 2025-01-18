1cb38937e858306eb5ff4e2d28f8a3d007a544b0
Patrick Beaume <p.beaume@gmail.com>
2025-01-18 15:20:22 +0100

    add box

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
   return (
     <>
       { !!game && <GameDisplay players={game.players.length} /> }
-      <LoadingScreen display={loading} author="Someone" artist="Somebody" publisher="Nobody" developer="You"/>
+      <LoadingScreen display={loading} author="Florian Grenier" artist="Valériane Holley" publisher="Grrre Games" developer="Game Park"/>
       <MaterialHeader rulesStepsHeaders={Headers} loading={loading}/>
       <MaterialImageLoader onImagesLoad={() => setImagesLoading(false)} />
       <Menu/>
