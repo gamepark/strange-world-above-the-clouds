581c1398c383560d63a8f1a8f2ae49cd8fc8bc7a
Patrick Beaume <p.beaume@gmail.com>
2025-01-22 15:23:08 +0100

    add clic on player panel

--- app/src/panels/PlayerPanels.tsx
+++ app/src/panels/PlayerPanels.tsx
@@ -1,25 +1,55 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
-import { getRelativePlayerIndex, StyledPlayerPanel, useGame, useMaterialContext, usePlayers } from '@gamepark/react-game'
-import { MaterialGame } from '@gamepark/rules-api'
+import { getRelativePlayerIndex, StyledPlayerPanel, useFocusContext, useMaterialContext, usePlayerId, usePlayers, useRules } from '@gamepark/react-game'
+import { MaterialRules } from '@gamepark/rules-api'
+import { LocationType } from '@gamepark/strange-world-above-the-clouds/material/LocationType'
+import { MaterialType } from '@gamepark/strange-world-above-the-clouds/material/MaterialType'
 import { PlayerColor } from '@gamepark/strange-world-above-the-clouds/PlayerColor'
 import { ScoringHelper } from '@gamepark/strange-world-above-the-clouds/rules/helpers/ScoringHelper'
+import { useCallback } from 'react'
 import { createPortal } from 'react-dom'
 import Star from '../images/icons/star.png'
-import { playerPositions, Position } from '../locators/TableauLocator'
+import EarthPanel from '../images/panel/earth.jpg'
+import MountainPanel from '../images/panel/mountain.jpg'
 
 
 import SwampPanel from '../images/panel/swamp.jpg'
 import WaterPanel from '../images/panel/water.jpg'
-import EarthPanel from '../images/panel/earth.jpg'
-import MountainPanel from '../images/panel/mountain.jpg'
+import { playerPositions, Position } from '../locators/TableauLocator'
 
 export const PlayerPanels = () => {
   const players = usePlayers<PlayerColor>({ sortFromMe: true })
-  const game = useGame<MaterialGame>()!
+  const rules = useRules<MaterialRules>()!
+  const game = rules.game
   const root = document.getElementById('root')
   const context = useMaterialContext()
   const isEnded = game.rule === undefined
+  const playerId = usePlayerId()
+  const { setFocus } = useFocusContext()
+
+  const onPlayerClick = useCallback((player) => {
+    const itsMe = player === playerId
+      setFocus({
+        materials: itsMe? [
+          rules.material(MaterialType.LandCard).player(player),
+          rules.material(MaterialType.LandCard).location(LocationType.LandDeck),
+          rules.material(MaterialType.LandCard).location(LocationType.FumaroleStack),
+          rules.material(MaterialType.TravelerCard).location(LocationType.TravelerStack),
+          rules.material(MaterialType.DarkCityCard).location(LocationType.DarkCityStack)
+        ]: [
+          rules.material(MaterialType.LandCard).player(player)
+        ],
+        staticItems: [],
+        locations: [],
+        animationTime: 500,
+        margin: {
+          top: 5,
+          bottom: 5,
+          left: 5
+        }
+      })
+  }, [rules, playerId])
+
   if (!root) {
     return null
   }
@@ -30,12 +60,19 @@ export const PlayerPanels = () => {
           const playerIndex = getRelativePlayerIndex(context, player.id)
           const position = playerPositions[context.rules.players.length - 2][playerIndex]
 
-          return <StyledPlayerPanel key={player.id} player={player} color={playerColorCode[player.id]} css={[panelPosition, getPositionCss(position)]}
-                                    backgroundImage={getPlayerBackground(player.id)}
-                                    mainCounter={isEnded?  {
-                                      image: Star,
-                                      value: new ScoringHelper(game, player.id).score
-                                    }: undefined}/>
+
+          return <StyledPlayerPanel
+            key={player.id}
+            player={player}
+            color={playerColorCode[player.id]}
+            onClick={() => onPlayerClick(player.id)}
+            css={[panelPosition, getPositionCss(position)]}
+            backgroundImage={getPlayerBackground(player.id)}
+            mainCounter={isEnded ? {
+              image: Star,
+              value: new ScoringHelper(game, player.id).score
+            } : undefined}
+          />
         }
       )}
     </>,
@@ -50,13 +87,13 @@ const panelPosition = css`
 const getPlayerBackground = (player: PlayerColor) => {
   switch (player) {
     case PlayerColor.Blue:
-      return WaterPanel;
+      return WaterPanel
     case PlayerColor.Gray:
-      return MountainPanel;
+      return MountainPanel
     case PlayerColor.Green:
-      return EarthPanel;
+      return EarthPanel
     case PlayerColor.Yellow:
-      return SwampPanel;
+      return SwampPanel
 
   }
 }
