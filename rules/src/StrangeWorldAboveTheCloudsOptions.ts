c3378ec1772293b28c721361fe23f63a67c911e2
Patrick Beaume <p.beaume@gmail.com>
2025-02-09 20:56:18 +0100

    fix: add player options to randomize the starting for player

--- rules/src/StrangeWorldAboveTheCloudsOptions.ts
+++ rules/src/StrangeWorldAboveTheCloudsOptions.ts
@@ -1,7 +1,30 @@
 import { OptionsSpec } from '@gamepark/rules-api'
+import { TFunction } from 'i18next'
+import { PlayerColor, playerColors } from './PlayerColor'
 
+/**
+ * This is the options for each player in the game.
+ */
+type PlayerOptions = { id: PlayerColor }
+
+/**
+ * This is the type of object that the game receives when a new game is started.
+ * The first generic parameter, "{}", can be changed to include game options like variants or expansions.
+ */
 export type StrangeWorldAboveTheCloudsOptions = {
-  players: number
+  players: PlayerOptions[]
 }
+
+/**
+ * This object describes all the options a game can have, and will be used by GamePark website to create automatically forms for you game
+ * (forms for friendly games, or forms for matchmaking preferences, for instance).
+ */
 export const StrangeWorldAboveTheCloudsOptionsSpec: OptionsSpec<StrangeWorldAboveTheCloudsOptions> = {
+  players: {
+    id: {
+      label: (t: TFunction) => t('player.color'),
+      values: playerColors,
+      valueSpec: color => ({ label: t => t(`player.${color}`)})
+    }
+  }
 }
